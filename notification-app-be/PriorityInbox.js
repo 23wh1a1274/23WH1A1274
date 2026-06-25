c21bd13668e1a3b6d2d42d1fdef56c4b3488236a
tanishqa <tanishqakalidindi@gmail.com>
2026-06-25 15:42:42 +0530

    Stage 6 - top 10 generated + Img of console added

--- notification-app-be/PriorityInbox.js
+++ notification-app-be/PriorityInbox.js
@@ -1,14 +1,8 @@
-const ACCESS_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyM3doMWExMjc0QGJ2cml0aHlkZXJhYmFkLmVkdS5pbiIsImV4cCI6MTc4MjM4MTg4OCwiaWF0IjoxNzgyMzgwOTg4LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiN2I4ZTYyNDQtYzU0Yy00ZjkwLWIwNDMtNmRkMWM4ZDRiNTk2IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidGFuaXNocWEga2FsaWRpbmRpIiwic3ViIjoiZWJlNTgxOTQtZTJiYi00YmY2LTk1YTUtMTMwYTQyMzIzMWYzIn0sImVtYWlsIjoiMjN3aDFhMTI3NEBidnJpdGh5ZGVyYWJhZC5lZHUuaW4iLCJuYW1lIjoidGFuaXNocWEga2FsaWRpbmRpIiwicm9sbE5vIjoiMjN3aDFhMTI3NCIsImFjY2Vzc0NvZGUiOiJhaFhqdnAiLCJjbGllbnRJRCI6ImViZTU4MTk0LWUyYmItNGJmNi05NWE1LTEzMGE0MjMyMzFmMyIsImNsaWVudFNlY3JldCI6InRjV0ZudnpHeFdoVFBYdEIifQ.rGQpTEfIO4wDi1a9eM_5FaP7DrU6TK9qx5OcnIVz6UM";
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyM3doMWExMjc0QGJ2cml0aHlkZXJhYmFkLmVkdS5pbiIsImV4cCI6MTc4MjM4Mjg2MSwiaWF0IjoxNzgyMzgxOTYxLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZGRjZDNjMmItYjU5Zi00NTI0LWIxZDAtNWJlODE5NjhjMjA5IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoidGFuaXNocWEga2FsaWRpbmRpIiwic3ViIjoiZWJlNTgxOTQtZTJiYi00YmY2LTk1YTUtMTMwYTQyMzIzMWYzIn0sImVtYWlsIjoiMjN3aDFhMTI3NEBidnJpdGh5ZGVyYWJhZC5lZHUuaW4iLCJuYW1lIjoidGFuaXNocWEga2FsaWRpbmRpIiwicm9sbE5vIjoiMjN3aDFhMTI3NCIsImFjY2Vzc0NvZGUiOiJhaFhqdnAiLCJjbGllbnRJRCI6ImViZTU4MTk0LWUyYmItNGJmNi05NWE1LTEzMGE0MjMyMzFmMyIsImNsaWVudFNlY3JldCI6InRjV0ZudnpHeFdoVFBYdEIifQ.fJ1xAy1P5hsE80IGPYy372LZDEOFjYDAYFHlJV0bUFw";
 
 const API_URL = "http://4.224.186.213/evaluation-service/notifications";
 
-
-const priorityplacement = {
-  Placement: 3,
-  Result: 2,
-  Event: 1,
-};
-async function fetchNotifications() {
+async function getPriorityNotifications() {
   try {
     const response = await fetch(API_URL, {
       headers: {
@@ -16,19 +10,34 @@ async function fetchNotifications() {
       }
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP Error: ${response.status}`);
-    }
+    
 
-    const data = 
-    await response.json();
+    const data = await response.json();
+    console.log(data);
 
-    return data.notifications;
+    let notifications = data.notifications;
+    notifications = notifications.filter(notification => !notification.isRead);
+
+    notifications.sort((a, b) => {
+      const priority = {
+        Placement: 3,
+        Result: 2,
+        Event: 1
+      };
+
+      if (priority[a.notificationType] !== priority[b.notificationType]) {
+        return priority[b.notificationType] - priority[a.notificationType];
+      }
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    });
+    const top10 = notifications.slice(0, 10);
+
+    console.log("The top 10 notifications");
+    console.log(top10);
+
   } catch (error) {
-    console.error("Error fetching notifications:", error.message);
+    console.log("Error:", error.message);
   }
 }
 
-fetchNotifications().then((notifications) => {
-  console.log(notifications);
-});
+getPriorityNotifications();
